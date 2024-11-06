89bd1072b087c537ba166f39431c156ef5ff0194
Shiji Yang <yangshiji66@qq.com>
2024-11-06 23:05:31 +0800

    device: add support for Phicomm K2(G)
    Signed-off-by: Shiji Yang <yangshiji66@qq.com>

---
+++ trunk/configs/boards/Phicomm_K2/board.h
@@ -0,0 +1,34 @@
+/* Phicomm K2 */
+
+#define BOARD_PID                "Phicomm_K2"
+#define BOARD_NAME               "Phicomm K2"
+#define BOARD_DESC               "Phicomm K2 Wireless Router"
+#define BOARD_VENDOR_NAME        "Phicomm"
+#define BOARD_VENDOR_URL         "http://www.phicomm.com/"
+#define BOARD_MODEL_URL          "http://www.phicomm.com/"
+#define BOARD_BOOT_TIME          30
+#define BOARD_FLASH_TIME         75
+#define BOARD_GPIO_BTN_RESET     1
+#undef  BOARD_GPIO_BTN_WPS
+#undef  BOARD_GPIO_BTN_FN1
+#undef  BOARD_GPIO_BTN_ROUTER
+#undef  BOARD_GPIO_LED_ALL
+#define BOARD_GPIO_LED_WIFI      11 /* YELLOW ACTIVE_LOW */
+#undef  BOARD_GPIO_LED_SW2G
+#undef  BOARD_GPIO_LED_SW5G
+#define BOARD_GPIO_LED_POWER     8  /* RED ACTIVE_HIGH */
+#undef  BOARD_GPIO_LED_LAN
+#define BOARD_GPIO_LED_WAN       10 /* BLUE ACTIVE_LOW */
+#undef  BOARD_GPIO_LED_USB
+#undef  BOARD_GPIO_LED_ROUTER
+#undef  BOARD_GPIO_PWR_USB
+#undef  BOARD_GPIO_PWR_USB_ON
+#define BOARD_HAS_5G_11AC        1
+#define BOARD_NUM_ANT_5G_TX      2
+#define BOARD_NUM_ANT_5G_RX      2
+#define BOARD_NUM_ANT_2G_TX      2
+#define BOARD_NUM_ANT_2G_RX      2
+#define BOARD_NUM_ETH_LEDS       0
+#define BOARD_NUM_ETH_EPHY       5
+#define BOARD_HAS_EPHY_L1000     0
+#define BOARD_HAS_EPHY_W1000     0
