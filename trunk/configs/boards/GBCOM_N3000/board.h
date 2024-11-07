bc2e9a5d4a2b21e05967f1742e019cebfa7472be
Shiji Yang <yangshiji66@qq.com>
2024-11-07 16:08:31 +0000

    device: add support for GBCOM N3000
    Signed-off-by: Shiji Yang <yangshiji66@qq.com>

---
+++ trunk/configs/boards/GBCOM_N3000/board.h
@@ -0,0 +1,34 @@
+/* GBCOM N3000 */
+
+#define BOARD_PID                "GBCOM_N3000"
+#define BOARD_NAME               "GBCOM N3000-06"
+#define BOARD_DESC               "GBCOM N3000-06 Wireless Router"
+#define BOARD_VENDOR_NAME        "GBCOM"
+#define BOARD_VENDOR_URL         "https://www.gbcom.com.cn/"
+#define BOARD_MODEL_URL          "https://www.gbcom.com.cn/"
+#define BOARD_BOOT_TIME          30
+#define BOARD_FLASH_TIME         75
+#define BOARD_GPIO_BTN_RESET     1
+#undef  BOARD_GPIO_BTN_WPS
+#undef  BOARD_GPIO_BTN_FN1
+#undef  BOARD_GPIO_BTN_ROUTER
+#undef  BOARD_GPIO_LED_ALL
+#undef  BOARD_GPIO_LED_WIFI
+#undef  BOARD_GPIO_LED_SW2G
+#undef  BOARD_GPIO_LED_SW5G
+#define BOARD_GPIO_LED_POWER     72
+#undef  BOARD_GPIO_LED_LAN
+#undef  BOARD_GPIO_LED_WAN
+#undef  BOARD_GPIO_LED_USB
+#undef  BOARD_GPIO_LED_ROUTER
+#undef  BOARD_GPIO_PWR_USB
+#undef  BOARD_GPIO_PWR_USB_ON
+#define BOARD_HAS_5G_11AC        1
+#define BOARD_NUM_ANT_5G_TX      2
+#define BOARD_NUM_ANT_5G_RX      2
+#define BOARD_NUM_ANT_2G_TX      2
+#define BOARD_NUM_ANT_2G_RX      2
+#define BOARD_NUM_ETH_LEDS       0
+#define BOARD_NUM_ETH_EPHY       2
+#define BOARD_HAS_EPHY_L1000     0
+#define BOARD_HAS_EPHY_W1000     0
