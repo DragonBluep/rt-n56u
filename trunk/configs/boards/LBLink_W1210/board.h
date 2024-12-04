4741c73309249bd4cb8ec1a9f4c759fa2026ac7b
Shiji Yang <yangshiji66@qq.com>
2024-12-04 13:42:45 +0800

    device: add support for LB-LINK BL-W1210
    Signed-off-by: Shiji Yang <yangshiji66@qq.com>

---
+++ trunk/configs/boards/LBLink_W1210/board.h
@@ -0,0 +1,34 @@
+/* LB-LINK BL-W1210 */
+
+#define BOARD_PID                "LBLink_W1210"
+#define BOARD_NAME               "LB-LINK BL-W1210"
+#define BOARD_DESC               "LB-LINK BL-W1210 Wireless Router"
+#define BOARD_VENDOR_NAME        "LB-LINK"
+#define BOARD_VENDOR_URL         "https://www.b-link.net.cn/"
+#define BOARD_MODEL_URL          "https://www.b-link.net.cn/"
+#define BOARD_BOOT_TIME          35
+#define BOARD_FLASH_TIME         90
+#define BOARD_GPIO_BTN_RESET     13
+#undef  BOARD_GPIO_BTN_WPS
+#undef  BOARD_GPIO_BTN_FN1
+#undef  BOARD_GPIO_BTN_ROUTER
+#undef  BOARD_GPIO_LED_ALL
+#undef  BOARD_GPIO_LED_WIFI
+#undef  BOARD_GPIO_LED_SW2G
+#undef  BOARD_GPIO_LED_SW5G
+#undef  BOARD_GPIO_LED_POWER
+#undef  BOARD_GPIO_LED_LAN
+#define BOARD_GPIO_LED_WAN       39
+#undef  BOARD_GPIO_LED_USB
+#undef  BOARD_GPIO_LED_ROUTER
+#undef  BOARD_GPIO_PWR_USB
+#undef  BOARD_GPIO_PWR_USB_ON
+#define BOARD_HAS_5G_11AC        1
+#define BOARD_NUM_ANT_5G_TX      2
+#define BOARD_NUM_ANT_5G_RX      2
+#define BOARD_NUM_ANT_2G_TX      2
+#define BOARD_NUM_ANT_2G_RX      2
+#define BOARD_NUM_ETH_LEDS       1
+#define BOARD_NUM_ETH_EPHY       5
+#define BOARD_HAS_EPHY_L1000     1
+#define BOARD_HAS_EPHY_W1000     1
