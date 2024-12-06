11096512d2a34f2e417fb8efa5e76b8be48ff0aa
Shiji Yang <yangshiji66@qq.com>
2024-12-06 17:22:36 +0800

    device: add support for Youku X2
    Signed-off-by: Shiji Yang <yangshiji66@qq.com>

---
+++ trunk/configs/boards/Youku_X2/board.h
@@ -0,0 +1,34 @@
+/* Youku X2 */
+
+#define BOARD_PID                "Youku_X2"
+#define BOARD_NAME               "Youku X2"
+#define BOARD_DESC               "Youku X2 Wireless Router"
+#define BOARD_VENDOR_NAME        "Youku"
+#define BOARD_VENDOR_URL         "https://www.youku.com/"
+#define BOARD_MODEL_URL          "https://www.youku.com/"
+#define BOARD_BOOT_TIME          35
+#define BOARD_FLASH_TIME         90
+#define BOARD_GPIO_BTN_RESET     1
+#undef  BOARD_GPIO_BTN_WPS
+#undef  BOARD_GPIO_BTN_FN1
+#undef  BOARD_GPIO_BTN_ROUTER
+#undef  BOARD_GPIO_LED_ALL
+#undef  BOARD_GPIO_LED_WIFI
+#undef  BOARD_GPIO_LED_SW2G
+#undef  BOARD_GPIO_LED_SW5G
+#define BOARD_GPIO_LED_POWER     29
+#undef  BOARD_GPIO_LED_LAN
+#define BOARD_GPIO_LED_WAN       35
+#undef  BOARD_GPIO_LED_USB
+#undef  BOARD_GPIO_LED_ROUTER
+#undef  BOARD_GPIO_PWR_USB
+#undef  BOARD_GPIO_PWR_USB_ON
+#define BOARD_HAS_5G_11AC        1
+#define BOARD_NUM_ANT_5G_TX      2
+#define BOARD_NUM_ANT_5G_RX      2
+#define BOARD_NUM_ANT_2G_TX      2
+#define BOARD_NUM_ANT_2G_RX      2
+#define BOARD_NUM_ETH_LEDS       1
+#define BOARD_NUM_ETH_EPHY       3
+#define BOARD_HAS_EPHY_L1000     0
+#define BOARD_HAS_EPHY_W1000     0
