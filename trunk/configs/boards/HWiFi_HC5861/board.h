d3291126c69f8286787dc3ba654a38e3441d9e63
Shiji Yang <yangshiji66@qq.com>
2024-12-06 17:21:45 +0800

    device: add support for HiWiFi HC5861
    Signed-off-by: Shiji Yang <yangshiji66@qq.com>

---
+++ trunk/configs/boards/HWiFi_HC5861/board.h
@@ -0,0 +1,36 @@
+/* HiWiFi HC5861 */
+
+#define BOARD_PID                "HWiFi_HC5861"
+#define BOARD_NAME               "HiWiFi HC5861"
+#define BOARD_DESC               "HiWiFi HC5861 Wireless Router"
+#define BOARD_VENDOR_NAME        "HiWiFi"
+#define BOARD_VENDOR_URL         "http://www.hiwifi.com/"
+#define BOARD_MODEL_URL          "http://www.hiwifi.com/"
+#define BOARD_BOOT_TIME          35
+#define BOARD_FLASH_TIME         90
+#define BOARD_GPIO_BTN_RESET     12
+#undef  BOARD_GPIO_BTN_WPS
+#undef  BOARD_GPIO_BTN_FN1
+#undef  BOARD_GPIO_BTN_ROUTER
+#undef  BOARD_GPIO_LED_ALL
+#undef  BOARD_GPIO_LED_WIFI
+#define BOARD_GPIO_LED_SW2G      11
+#define BOARD_GPIO_LED_SW5G      7
+#define BOARD_GPIO_LED_POWER     9
+#undef  BOARD_GPIO_LED_LAN
+#define BOARD_GPIO_LED_WAN       72
+#define BOARD_GPIO_LED_USB       10
+#undef  BOARD_GPIO_LED_ROUTER
+#define BOARD_GPIO_PWR_MMC       8
+#define BOARD_GPIO_PWR_MMC_ON    0
+#define BOARD_GPIO_PWR_USB       13
+#define BOARD_GPIO_PWR_USB_ON    0
+#define BOARD_HAS_5G_11AC        1
+#define BOARD_NUM_ANT_5G_TX      2
+#define BOARD_NUM_ANT_5G_RX      2
+#define BOARD_NUM_ANT_2G_TX      2
+#define BOARD_NUM_ANT_2G_RX      2
+#define BOARD_NUM_ETH_LEDS       0
+#define BOARD_NUM_ETH_EPHY       3
+#define BOARD_HAS_EPHY_L1000     0
+#define BOARD_HAS_EPHY_W1000     1
