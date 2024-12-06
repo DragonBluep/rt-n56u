ddd56ebe1df440ced763e73d87799793e8204697
Shiji Yang <yangshiji66@qq.com>
2024-12-06 17:12:46 +0800

    device: add support for Domywifi DM203
    Signed-off-by: Shiji Yang <yangshiji66@qq.com>

---
+++ trunk/configs/boards/Domy_DM203/board.h
@@ -0,0 +1,36 @@
+/* Domywifi DM203 */
+
+#define BOARD_PID                "Domy_DM203"
+#define BOARD_NAME               "Domywifi DM203"
+#define BOARD_DESC               "Domywifi DM203 Wireless Router"
+#define BOARD_VENDOR_NAME        "Domywifi"
+#define BOARD_VENDOR_URL         "http://www.domytech.cn/"
+#define BOARD_MODEL_URL          "http://www.domytech.cn/"
+#define BOARD_BOOT_TIME          35
+#define BOARD_FLASH_TIME         90
+#define BOARD_GPIO_BTN_RESET     13
+#undef  BOARD_GPIO_BTN_WPS
+#undef  BOARD_GPIO_BTN_FN1
+#undef  BOARD_GPIO_BTN_ROUTER
+#undef  BOARD_GPIO_LED_ALL
+#undef  BOARD_GPIO_LED_WIFI
+#undef  BOARD_GPIO_LED_SW2G
+#undef  BOARD_GPIO_LED_SW5G
+#define BOARD_GPIO_LED_POWER     30 /* green */
+#define BOARD_GPIO_LED_POWER1    26 /* amber */
+#undef  BOARD_GPIO_LED_LAN
+#define BOARD_GPIO_LED_WAN       24 /* green */
+#define BOARD_GPIO_LED_WAN1      29 /* amber */
+#undef  BOARD_GPIO_LED_USB
+#undef  BOARD_GPIO_LED_ROUTER
+#undef  BOARD_GPIO_PWR_USB
+#undef  BOARD_GPIO_PWR_USB_ON
+#define BOARD_HAS_5G_11AC        1
+#define BOARD_NUM_ANT_5G_TX      1
+#define BOARD_NUM_ANT_5G_RX      1
+#define BOARD_NUM_ANT_2G_TX      2
+#define BOARD_NUM_ANT_2G_RX      2
+#define BOARD_NUM_ETH_LEDS       1
+#define BOARD_NUM_ETH_EPHY       5
+#define BOARD_HAS_EPHY_L1000     0
+#define BOARD_HAS_EPHY_W1000     0
