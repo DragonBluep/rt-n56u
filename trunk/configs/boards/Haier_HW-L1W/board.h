6252c99d81b11b9487172b3e3e8832bf09d60828
Shiji Yang <yangshiji66@qq.com>
2024-12-06 17:14:44 +0800

    device: add support for Haier HW-L1W
    Signed-off-by: Shiji Yang <yangshiji66@qq.com>

---
+++ trunk/configs/boards/Haier_HW-L1W/board.h
@@ -0,0 +1,34 @@
+/* Haier HW-L1W */
+
+#define BOARD_PID                "Haier_HW-L1W"
+#define BOARD_NAME               "Haier HW-L1W"
+#define BOARD_DESC               "Haier HW-L1W Wireless Router"
+#define BOARD_VENDOR_NAME        "Haier"
+#define BOARD_VENDOR_URL         "https://www.haier.com/"
+#define BOARD_MODEL_URL          "https://www.haier.com/"
+#define BOARD_BOOT_TIME          35
+#define BOARD_FLASH_TIME         90
+#define BOARD_GPIO_BTN_RESET     13
+#undef  BOARD_GPIO_BTN_WPS
+#undef  BOARD_GPIO_BTN_FN1
+#undef  BOARD_GPIO_BTN_ROUTER
+#undef  BOARD_GPIO_LED_ALL
+#undef  BOARD_GPIO_LED_WIFI
+#undef  BOARD_GPIO_LED_SW2G
+#undef  BOARD_GPIO_LED_SW5G
+#undef  BOARD_GPIO_LED_POWER
+#undef  BOARD_GPIO_LED_LAN
+#define BOARD_GPIO_LED_WAN       11
+#undef  BOARD_GPIO_LED_USB
+#undef  BOARD_GPIO_LED_ROUTER
+#undef  BOARD_GPIO_PWR_USB
+#undef  BOARD_GPIO_PWR_USB_ON
+#define BOARD_HAS_5G_11AC        1
+#define BOARD_NUM_ANT_5G_TX      1
+#define BOARD_NUM_ANT_5G_RX      1
+#define BOARD_NUM_ANT_2G_TX      2
+#define BOARD_NUM_ANT_2G_RX      2
+#define BOARD_NUM_ETH_LEDS       1
+#define BOARD_NUM_ETH_EPHY       3
+#define BOARD_HAS_EPHY_L1000     0
+#define BOARD_HAS_EPHY_W1000     0
