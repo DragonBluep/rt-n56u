870635635ee6943a25b1f16c1cafe61c37ec6e28
Shiji Yang <yangshiji66@qq.com>
2025-05-26 18:00:09 +0800

    user: enable firewall SYN and DoS protection by default
    Signed-off-by: Shiji Yang <yangshiji66@qq.com>

--- trunk/user/shared/defaults.c
+++ trunk/user/shared/defaults.c
@@ -148,7 +148,7 @@ struct nvram_pair router_defaults[] = {
 	{ "http_lanport", "80" },		/* HTTP LAN port to listen on */
 	{ "https_lport", "443" },		/* HTTPS LAN port to listen on */
 	{ "https_clist", DEF_HTTPS_CIPH_LIST },	/* HTTPS SSL cipher list */
-	{ "fw_dos_x", "0" },			// oleg patch
+	{ "fw_dos_x", "1" },			// oleg patch
 	{ "dr_enable_x", "1" },			// oleg patch
 	{ "mr_enable_x", "0" },			// oleg patch
 	{ "mr_qleave_x", "1" },
@@ -785,7 +785,7 @@ struct nvram_pair router_defaults[] = {
 #if defined(USE_SFE)
 	{ "sfe_enable", "0" },
 #endif
-	{ "fw_syn_cook", "0" },
+	{ "fw_syn_cook", "1" },
 	{ "fw_mac_drop", "0" },
 	{ "nf_nat_type", "2" },
 	{ "nf_nat_loop", "1" },
